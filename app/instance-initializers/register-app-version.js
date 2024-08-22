69e4a5196d8d9920bdc85d3e82f6fb4ca89cbaf3
Francis Muturi <francmut@gmail.com>
2024-08-22 16:55:42 +0300

    Update verification code length

---
+++ app/instance-initializers/register-app-version.js
@@ -0,0 +1,18 @@
+export function initialize() {
+    // const universe = owner.lookup('service:universe');
+    // if (universe) {
+    //     universe.registerOrganizationMenuItem(`v${config.version}`, {
+    //         index: 4,
+    //         route: null,
+    //         icon: 'code-branch',
+    //         iconSize: 'xs',
+    //         iconClass: 'mr-1.5',
+    //         wrapperClass: 'app-version-in-nav',
+    //         overwriteWrapperClass: true,
+    //     });
+    // }
+}
+
+export default {
+    initialize,
+};
